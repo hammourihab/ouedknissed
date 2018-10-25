5ba38d95895970d7739af13380f5ff1173f1af2d
Hammour ihab <hammourihab@yandex.com>
2018-10-25 21:27:23 +0100

    updated ouedknissed with changes

--- application/views/home.php
+++ application/views/home.php
@@ -1,89 +1,52 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
+	<title>Ouedknissed.com| Re-inventing a new UI/UX for ouedkniss</title>
+	<link rel="icon" href="<?= base_url('/assets/images/favicon.ico'); ?>">
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+	<meta name="keywords" content="ouedkniss, UI/UX, Semantic-UI, ouedknissed">
+	<meta name="description" content="Re-inventing a new UI/UX for ouedkniss">
+	<meta name="author" content="hammourihab">
+	<meta name="application-name" content="ouedknissed">
+	<meta name="google-site-verification" content="" />
+	<meta name="theme-color" content="">
+	<meta name="msapplication-navbutton-color" content="">
+	<meta name="apple-mobile-web-app-status-bar-style" content="">
+	<meta name="msapplication-TileColor" content="">
+	<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js">
+			</script>
+		-->
+	<meta property="og:url" content="<?= base_url(); ?>">
+	<meta property="og:title" content="Ouedknissed.com|">
+	<meta property="og:image" content="">
+	<meta property="og:type" content="website">
+	<meta property="og:description" content="Re-inventing a new UI/UX for ouedkniss">
+	<meta property="og:site_name" content="ouedknissed" />
+	<meta property="fb:app_id" content="">
+	
+	<meta itemprop="name" content="ouedknissed">
+	<meta itemprop="description" content="Re-inventing a new UI/UX for ouedkniss">
+	<meta itemprop="image" content="">
+	
+	<meta name="twitter:card" content="">
+	<meta name="twitter:site" content="<?= base_url(); ?>">
+	<meta name="twitter:title" content="Ouedknissed.com|">
+	<meta name="twitter:description" content="Re-inventing a new UI/UX for ouedkniss">
+	<meta name="twitter:creator" content="hammourihab">
+	<meta name="twitter:image:src" content="">
 
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<link rel="stylesheet" href="<?= base_url('/assets/semantic/semantic.min.css'); ?>">
+	<link rel="stylesheet" href="<?= base_url('/assets/css/owl.carousel.min.css'); ?>">
+	<link rel="stylesheet" href="<?= base_url('/assets/css/owl.theme.default.min.css'); ?>">
+	<link rel="stylesheet" href="<?= base_url('/assets/css/app.css'); ?>">
+	<script src="<?= base_url('/assets/js/jquery-3.2.1.min.js'); ?>" type='text/javascript'></script>
+	<script src="<?= base_url('/assets/semantic/semantic.min.js'); ?>" type='text/javascript'></script>
+	<script src="<?= base_url('/assets/js/owl.carousel.min.js'); ?>" type="text/javascript"></script>
+	<script src="<?= base_url('/assets/js/app.js'); ?>" type='text/javascript'></script>
 </head>
+
 <body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
-</body>
-</html>
+	<?php $this->load->view('layouts/navigation'); ?>	
---
+++ application/views/layouts/navigation.php
@@ -0,0 +1,12 @@
+<!-- Large screens menu -->
+<div class="ui top fixed borderless tiny menu" id="menu">
+	<div class="item">
+		<img src="<?= base_url('assets/images/logos/ouedknissed-200-50.png'); ?>" class="ui small image">
+	</div>
+</div>
+<!-- Small screens menu -->
+<div class="ui top fixed borderless tiny menu" id="small_screens_menu">
+	<div class="item">
+		<img src="<?= base_url('assets/images/logos/ouedknissed-200-50.png'); ?>" class="ui tiny image">
+	</div>
+</div>
