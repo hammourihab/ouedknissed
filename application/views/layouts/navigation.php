e2f48c85d2299c2f359c93482bac093d3426e9ca
Hammour ihab <hammourihab@yandex.com>
2018-11-01 22:33:06 +0100

    added sidebar

--- application/views/layouts/navigation.php
+++ application/views/layouts/navigation.php
@@ -1,12 +1,46 @@
+<div class="ui vertical right sidebar centered borderless menu" id="small_screens_sidebar">
+	<div class="item">
+			<img src="<?= base_url('assets/images/logos/ouedknissed-200-50.png'); ?>" class="ui small centered image">	
+	</div>
+</div>
+<div class="pusher">
+
+
+
 <!-- Large screens menu -->
 <div class="ui top fixed borderless tiny menu" id="menu">
-	<div class="item">
+	<div class="header item">
 		<img src="<?= base_url('assets/images/logos/ouedknissed-200-50.png'); ?>" class="ui small image">
+	</div>
+	<div class="right menu">
+		<div class="item">
+			<a href="" class="ui button ouedknissed-primary-btn"> <i class="paper plane icon"></i> Contact</a>
+		</div>
+		<div class="item">
+			<a href="" class="ui button ouedknissed-primary-btn">Publicité</a>
+		</div>
+		<div class="item">
+			<div class="ui simple dropdown item">
+				<i class="language icon"></i>
+					Language
+				<i class="dropdown icon"></i>
+				<div class="menu">
+					<a class="item" href="/en"><i class="us flag"></i>English</a>
+					<a class="item" href="/fr"><i class="fr flag"></i>Français</a>
+					<a class="item" href="/ar"><i class="uae flag"></i>العربية</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 <!-- Small screens menu -->
 <div class="ui top fixed borderless tiny menu" id="small_screens_menu">
-	<div class="item">
-		<img src="<?= base_url('assets/images/logos/ouedknissed-200-50.png'); ?>" class="ui tiny image">
+	<div class="header item">
+		<img src="<?= base_url('assets/images/logos/ouedknissed-200-50.png'); ?>" class="ui small image">
+	</div>
+	<div class="right menu">
+		<a class="toc item">
+			<i class="sidebar large primary icon"></i> 
+		</a>
 	</div>
 </div>
