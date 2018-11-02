8afcdab68ac4d42e7d8666fffb9d13a104010438
Hammour ihab <hammourihab@yandex.com>
2018-11-03 00:10:41 +0100

    added footer

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,82 @@
+	</div>
+  <footer class="ui vertical footer">
+		
+		<div class="ui stackable aligned grid container" style="padding-top: 5vh;">
+			<div class="ui four column row">
+				<div class="ui four wide column">
+					<div class="ui vertical link list">
+						<span class="item">
+							<a class="ui link white" href="#">Annonces Immobilier en Algérie</a>
+						</span>
+						<span class="item">
+							<a class="ui link white" href="#"></a>
+						</span>
+						<span class="item">
+							<a class="ui link white" href="#"></a>
+						</span>
+						<span class="item">
+							<a class="ui link white" href="#"></a>
+						</span>
+					</div>
+				</div>
+				<div class="ui four wide column">
+					<div class="ui medium inverted white header">
+					</div>
+					<div class="ui vertical link list">
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+					</div>
+				</div>
+				<div class="ui four wide column">
+					<div class="ui medium inverted white header">
+					  
+					</div>
+					<div class="ui vertical link list">
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+					</div>
+				</div>
+				<div class="ui four wide column">
+				  <div class="ui medium inverted white header">
+					 
+					</div>
+					<div class="ui vertical link list">
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+						<span class="item">
+							<a class="ui link secondary" href=""></a>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	</body>
+</html>
